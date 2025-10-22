b3dac37cbd230f7d496153eb6128916aa503a207
StenSOn27 <tarashevchik27@gmail.com>
2025-10-23 01:44:02 +0300

    change type hint in interface

--- app/interfaces/book_serializer_interface.py
+++ app/interfaces/book_serializer_interface.py
@@ -5,5 +5,5 @@ from app.interfaces.book_interface import BookInterface
 class BookSerializerInterface(BookInterface):
     @staticmethod
     @abstractmethod
-    def serialize(book: "Book") -> None:
+    def serialize(book: "Book") -> str:
         pass
