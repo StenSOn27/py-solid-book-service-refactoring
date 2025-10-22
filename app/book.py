56f1344123253bee6c383970b5ef7dd23ed09c9a
StenSOn27 <tarashevchik27@gmail.com>
2025-10-23 01:18:08 +0300

    add strategies attribute to book

--- app/book.py
+++ app/book.py
@@ -1,48 +1,24 @@
-import json
-import xml.etree.ElementTree as Et
-from app.interfaces import (
-    BookSerializerInterface,
-    BookDisplayInterface,
-    BookPrinterInterface,
-)
+from app.displayer import ConsoleBookDisplay, ReverseBookDisplay
+from app.printer import ConsoleBookPrinter, ReverseBookPrinter
+from app.serializer import JsonBookSerializer, XMLBookSerializer
 
 
-class Book(
-    BookDisplayInterface,
-    BookPrinterInterface,
-    BookSerializerInterface
-):
+class Book:
     def __init__(self, title: str, content: str) -> None:
         self.title = title
         self.content = content
 
-    def display(self, display_type: str) -> None:
-        if display_type == "console":
-            print(self.content)
-        elif display_type == "reverse":
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown display type: {display_type}")
-
-    def print_book(self, print_type: str) -> None:
-        if print_type == "console":
-            print(f"Printing the book: {self.title}...")
-            print(self.content)
-        elif print_type == "reverse":
-            print(f"Printing the book in reverse: {self.title}...")
-            print(self.content[::-1])
-        else:
-            raise ValueError(f"Unknown print type: {print_type}")
-
-    def serialize(self, serialize_type: str) -> str:
-        if serialize_type == "json":
-            return json.dumps({"title": self.title, "content": self.content})
-        elif serialize_type == "xml":
-            root = Et.Element("book")
-            title = Et.SubElement(root, "title")
-            title.text = self.title
-            content = Et.SubElement(root, "content")
-            content.text = self.content
-            return Et.tostring(root, encoding="unicode")
-        else:
-            raise ValueError(f"Unknown serialize type: {serialize_type}")
+        self.strategies = {
+            "display": {
+                "console": ConsoleBookDisplay.display,
+                "reverse": ReverseBookDisplay.display,
+            },
+            "print": {
+                "console": ConsoleBookPrinter.print_book,
+                "reverse": ReverseBookPrinter.print_book,
+            },
+            "serialize": {
+                "json": JsonBookSerializer.serialize,
+                "xml": XMLBookSerializer.serialize,
+            }
+        }
