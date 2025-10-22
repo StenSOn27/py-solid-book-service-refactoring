51f7d567b9632885f7797e4146c0f3224d77be26
StenSOn27 <tarashevchik27@gmail.com>
2025-10-23 01:25:34 +0300

    add staticmethod decorators to printer and displayer classes

--- app/displayer.py
+++ app/displayer.py
@@ -2,9 +2,11 @@ from app.interfaces.book_display_interface import BookDisplayInterface
 
 
 class ConsoleBookDisplay(BookDisplayInterface):
+    @staticmethod
     def display(book: "Book") -> None:
         print(book.content)
 
 class ReverseBookDisplay(BookDisplayInterface):
+    @staticmethod
     def display(book: "Book") -> None:
         print(book.content[::-1])
--- app/printer.py
+++ app/printer.py
@@ -2,12 +2,14 @@ from app.interfaces.book_printer_interface import BookPrinterInterface
 
 
 class ConsoleBookPrinter(BookPrinterInterface):
+    @staticmethod
     def print_book(book: "Book") -> None:
         print(f"Printing the book: {book.title}...")
         print(book.content)
 
 
 class ReverseBookPrinter(BookPrinterInterface):
+    @staticmethod    
     def print_book(book: "Book") -> None:
         print(f"Printing the book in reverse: {book.title}...")
         print(book.content[::-1])
