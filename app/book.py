2fff28568dabcaa9a3c38ea17561ac1e6ed12ff0
StenSOn27 <tarashevchik27@gmail.com>
2025-10-23 01:33:47 +0300

    move out book strategies to main file from book class

--- app/book.py
+++ app/book.py
@@ -1,24 +1,5 @@
-from app.displayer import ConsoleBookDisplay, ReverseBookDisplay
-from app.printer import ConsoleBookPrinter, ReverseBookPrinter
-from app.serializer import JsonBookSerializer, XMLBookSerializer
-
 
 class Book:
     def __init__(self, title: str, content: str) -> None:
         self.title = title
         self.content = content
-
-        self.strategies = {
-            "display": {
-                "console": ConsoleBookDisplay.display,
-                "reverse": ReverseBookDisplay.display,
-            },
-            "print": {
-                "console": ConsoleBookPrinter.print_book,
-                "reverse": ReverseBookPrinter.print_book,
-            },
-            "serialize": {
-                "json": JsonBookSerializer.serialize,
-                "xml": XMLBookSerializer.serialize,
-            }
-        }
--- app/main.py
+++ app/main.py
@@ -1,10 +1,29 @@
 from app.book import Book
+from app.displayer import ConsoleBookDisplay, ReverseBookDisplay
+from app.printer import ConsoleBookPrinter, ReverseBookPrinter
+from app.serializer import JsonBookSerializer, XMLBookSerializer
+
+
+BOOK_STRATEGIES = {
+    "display": {
+        "console": ConsoleBookDisplay.display,
+        "reverse": ReverseBookDisplay.display,
+    },
+    "print": {
+        "console": ConsoleBookPrinter.print_book,
+        "reverse": ReverseBookPrinter.print_book,
+    },
+    "serialize": {
+        "json": JsonBookSerializer.serialize,
+        "xml": XMLBookSerializer.serialize,
+    }
+}
 
 
 def main(book: Book, commands: list[tuple[str, str]]) -> None | str:
     for cmd, method_type in commands:
         try:
-            strategy = book.strategies[cmd][method_type]
+            strategy = BOOK_STRATEGIES[cmd][method_type]
             return strategy(book)
         except ValueError as er:
             print("ValueError", er)
